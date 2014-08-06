8cf83fcf47fe8e05e3aa72c5ccbf34facb93ee3b
Ben Petroski <ben@benpetroski.com>
2014-08-06 14:41:13 -0700

    Completed codecademy bitly api course... need to figure out how to use oauth for authenticating with the twitter api

--- tweetTweet/tweeting/tweet.py
+++ tweetTweet/tweeting/tweet.py
@@ -3,13 +3,17 @@
 '''
 
 from urllib2 import Request, urlopen, URLError
+from pprint import pprint
+from xml.dom import minidom
+import requests, json, twitter
 
 class kitty(object):
     def text(self):
         try:
-            response = urlopen(Request('http://placekitten.com'))
-            kitty = response.read()
-            print kitty[560:1050]
+#             response = urlopen(Request('http://placekitten.com'))
+#             kitty = response.read()
+            kitty = requests.get('http://placekitten.com')
+            print kitty.text[560:1050]
         except URLError, e:
             print 'No kitties. Error: ', e
     def pic(self):
@@ -20,16 +24,50 @@ class kitty(object):
         
 def hk():
     try:
-        response = urlopen(Request('http://thehofmeisterkink.com'))
-        hk = response.read()
-        print hk
+        hk = requests.get('http://thehofmeisterkink.com')
+        print hk.headers
     except URLError, e:
         print 'No kitties. Error: ', e
 
+def pets():
+    f = open('pets.txt', 'r')
+    pets = minidom.parseString(f.read())
+    f.close()
+    
+    names = pets.getElementsByTagName('name')
+    for name in names:
+        print name.firstChild.nodeValue
+
+def pets2():
+    f = open('pets2.txt', 'r')
+    pets = json.loads(f.read())
+    f.close()
+    
+    pprint(pets)
+
+def bitly():
+    query_params = {'access_token': 'API_KEY',
+                'link': 'http://bit.ly/1hrUPUa',
+                'unit': 'minute',
+                'units': 15}
+
+    endpoint = 'https://api-ssl.bitly.com/v3/link/referrers'
+    response = requests.get(endpoint, params= query_params)
+    
+    data = json.loads(response.content)
+    print data
+
+def bnptrsk_tweetTweet():
+    API_KEY='***'
+    endpoint='https://api.twitter.com/1.1/statuses/update.json'
+    
+    api = twitter.Api()
+    print api
+
 if __name__ == '__main__':
-    k=kitty()
-    k.text()
-    hk()
+    bnptrsk_tweetTweet()
+    
+
 
     
     
